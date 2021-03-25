30175e3211996de86371e433b13ec57c967f8ff6
mike <mikemihalchik@gmail.com>
2021-03-25 19:54:09 -0400

    up to login

---
+++ authenticate-me/frontend/src/App.js
@@ -0,0 +1,16 @@
+// frontend/src/App.js
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import LoginFormPage from './component/LoginFormPage';
+
+function App() {
+  return (
+    <Switch>
+      <Route path="/login">
+        <LoginFormPage />
+      </Route>
+    </Switch>
+  );
+}
+
+export default App;
